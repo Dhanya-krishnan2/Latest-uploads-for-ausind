9003797c6dc5b4fc5644b846afb2624d54621372
DHANYA KUNHIKRISHNAN <63437424+Dhanya-krishnan2@users.noreply.github.com>
2021-02-26 10:45:42 +0800

    added navbar names and organised the path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,7 +64,7 @@ function Navbar() {
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
-                Products
+                Contact us
               </Link>
             </li>
 
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import '../../App.css';
-import Form from '../Contact.js';
-// import Contact from '../Contact.js';
-// import SimpleContainer from '../Contact.js';
 
-export default function Services() {
-  return (<>
-
-         <Form/>
-
-    </>
-    );
+export default function Products() {
+  return <h1 className='products'>PRODUCTS</h1>;
 }
+
